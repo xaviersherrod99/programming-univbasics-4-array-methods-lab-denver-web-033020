c0b99a27b3d03a56f1fe72cdbb37b6cd5b2ebb6c
Xa Sherrod <xaviersherrod99@yahoo.com>
2020-03-17 20:25:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,9 +19,10 @@ end
 
 def using_last(array)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-
+array.last
 end
 
 def using_size(array)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.size
 end
